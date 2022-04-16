5d9f9a68d0488b368d236058b8920e0b82ae1436
jyeon <yjyshsj0217@gmail.com>
2022-04-17 04:29:45 +0900

    스캔 한다하하핳하하핳하하핳

--- app/src/main/java/com/example/wonderv2/AR/AR_camera.java
+++ app/src/main/java/com/example/wonderv2/AR/AR_camera.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.media.MediaSession2Service;
+import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -14,12 +16,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.wonderv2.R;
+import com.example.wonderv2.ml.BottleModel;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.schema.Model;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class AR_camera extends AppCompatActivity {
 
     TextView bottle_name;
     ImageView bottle_image;
     Button scanBtn;
+
+    int imageSize = 224;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,10 +56,84 @@ public class AR_camera extends AppCompatActivity {
         });
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
+
+    public void classifyImage(Bitmap image) {
+        try {
+            BottleModel model = BottleModel.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+            for (int i = 0; i < imageSize; i++) {
+                for (int j = 0; j < imageSize; j++) {
+                    int val = intValues[pixel++]; //RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF)*(1.f / 255.f));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF)*(1.f / 255.f));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
+
+
+
+                }
+            }
+
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            BottleModel.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+
+            float[] confidences = outputFeature0.getFloatArray();
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for(int i =0; i<confidences.length; i++){
+                if(confidences[i] > maxConfidence){
+                    maxConfidence = confidences[i];
+                    maxPos=i;
+                }
+            }
+
+            String[] classes = {"circle bottle", "square bottle"};
+            bottle_name.setText(classes[maxPos]);
+
+
+            String s ="";
+            for(int i = 0; i <classes.length; i++){
+                s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100);
+            }
+
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode==1 && resultCode==RESULT_OK){
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            int dimension = Math.min(image.getWidth(), image.getHeight());
+            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+            bottle_image.setImageBitmap(image);
+
+            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+
+
+            classifyImage(image);
 
         }
+        super.onActivityResult(requestCode, resultCode, data);
 
     }
+
+
+
 }
